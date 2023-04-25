6b0d6e1ea2b98c538b6e86a87d226faa0a163f42
Mohit Gayatri <mohitgayatri96@gmail.com>
2023-04-24 23:51:50 -0400

    add td3 and allow buffer size and learning starts in config

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import argparse
 import time
 import torch
 import numpy as np
-from stable_baselines3 import PPO, DDPG
+from stable_baselines3 import PPO, DDPG, TD3
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecCheckNan
 from stable_baselines3.common.noise import NormalActionNoise
@@ -115,6 +115,20 @@ if __name__ == "__main__":
                         device=device,
                         action_noise=action_noise,
                         tensorboard_log=logdir)
+        
+        elif model_algo == "TD3":
+            model = TD3(agent_config['model_type'],
+                        env,
+                        verbose=1,
+                        learning_rate=training_config['learning_rate'],
+                        batch_size=training_config['batch_size'],
+                        tau=agent_config['tau'],
+                        policy_kwargs=policy_kwargs,
+                        train_freq=train_freq,
+                        device=device,
+                        action_noise=action_noise,
+                        tensorboard_log=logdir)
+
         else:
             raise (ValueError, f"invalid model algo provided {model_algo}. Only PPO and DDPG are accepted")
 
@@ -126,6 +140,7 @@ if __name__ == "__main__":
             policy_kwargs.pop('n_critics')
             model = PPO.load(load_model_ckpt,
                              env,
+                             learning_starts=training_config['learning_starts'],
                              learning_rate=training_config['learning_rate'],
                              batch_size=training_config['batch_size'],
                              verbose=1,
@@ -136,6 +151,8 @@ if __name__ == "__main__":
             model = DDPG.load(load_model_ckpt,
                               env,
                               verbose=1,
+                              buffer_size=training_config['buffer_size'], 
+                              learning_starts=training_config['learning_starts'],
                               learning_rate=training_config['learning_rate'],
                               batch_size=training_config['batch_size'],
                               tau=agent_config['tau'],
@@ -144,6 +161,20 @@ if __name__ == "__main__":
                               device=device,
                               action_noise=action_noise,
                               tensorboard_log=logdir)
+        elif model_algo == "TD3":
+            model = TD3.load(load_model_ckpt,
+                    env,
+                    verbose=1,
+                    buffer_size=training_config['buffer_size'], 
+                    learning_starts=training_config['learning_starts'],
+                    learning_rate=training_config['learning_rate'],
+                    batch_size=training_config['batch_size'],
+                    tau=agent_config['tau'],
+                    policy_kwargs=policy_kwargs,
+                    train_freq=train_freq,
+                    device=device,
+                    action_noise=action_noise,
+                    tensorboard_log=logdir)
         else:
             raise (ValueError, f"invalid model algo provided {model_algo}. Only PPO and DDPG are accepted")
 
