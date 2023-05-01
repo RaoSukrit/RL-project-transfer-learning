dc651b2f1c6389dcb34df6ec1630111e1f533c63
Mohit Gayatri <mohitgayatri96@gmail.com>
2023-05-01 13:24:04 -0400

    clean up code v1

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 import os
 import yaml
 import argparse
-import time
 import torch
-import numpy as np
 from stable_baselines3 import PPO, DDPG, SAC
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecCheckNan
@@ -12,7 +10,7 @@ from stable_baselines3.common.noise import NormalActionNoise
 from callbacks import *
 from utils import *
 
-# set MUJOCO_GL environment var
+# Set MUJOCO_GL environment var
 os.environ['MUJOCO_GL'] = "osmesa"
 os.environ.get('MUJOCO_GL', 'MUJOCO_GL not set')
 
@@ -20,41 +18,40 @@ os.environ.get('MUJOCO_GL', 'MUJOCO_GL not set')
 device = torch.device('cuda') if torch.cuda.is_available() \
     else torch.device('cpu')
 
-# print device
+# Print device
 print('*' * 50)
 print(f"using device: {device}")
 print('*' * 50)
 
 if __name__ == "__main__":
+    # add config argument  
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--config', type=str,
                         default='./configs/config.yaml',
                         help='Path to config file specifiying user params')
 
+    # get config and read 
     args = parser.parse_args()
-
     with open(args.config, 'r') as fh:
-        config = yaml.safe_load(fh)
-
-    job_timestamp = int(time.time())
+        cfg = yaml.safe_load(fh)
 
     # create log dir
-    logdir = make_log_dir(config, job_timestamp)
+    logdir = make_log_dir(cfg)
     print(f"Using logdir: {logdir}")
 
     # create env object
-    env = create_env(config)
+    env = create_env(cfg)
 
     # wrap env with Monitor
     env = Monitor(env, logdir)
 
-    training_config = config['training_params']
+    # read variables from training config
+    training_cfg = cfg['training_params']
+    do_resume_training = training_cfg['resume_training']
+    load_model_ckpt = training_cfg['load_model_ckpt_path']
+    is_transfer_learning = training_cfg['is_transfer_learning']
 
-    do_resume_training = training_config['resume_training']
-    load_model_ckpt = training_config['load_model_ckpt_path']
-    is_transfer_learning = training_config['is_transfer_learning']
-
+    # transfer learning with pre-trained model
     if do_resume_training and is_transfer_learning:
         print("*" * 50)
         print("PERFORMING TRANSFER LEARNING!\nCREATING MODEL FROM SCRATCH FOR NEW TASK")
@@ -63,9 +60,9 @@ if __name__ == "__main__":
         # create an env with the original domain and task on which the saved model
         # was trained. For this we need to modify the config and then
         # set the params for the feature extractor alone
-        pretrain_params = config['transfer_learning']
+        pretrain_params = cfg['transfer_learning']
 
-        pretrain_config = config['env_params']
+        pretrain_config = cfg['env_params']
         pretrain_config['domain_name'] = \
             pretrain_params['original_task']['domain_name']
         pretrain_config['task_name'] = \
@@ -76,14 +73,11 @@ if __name__ == "__main__":
         # create pretrained env, used to load the pretrained model
         pretrain_env = create_env({"env_params": pretrain_config})
 
-        # create model using checkpoint of pretrained rn
-        pretrain_model = reload_model_from_ckpt(pretrain_env,
-                                                config,
-                                                load_model_ckpt,
-                                                logdir)
+        # create model using checkpoint of pretrained model
+        pretrain_model = load_model(pretrain_env, cfg, logdir, load_model_ckpt)
 
         # create model using new environment. Used as base to reload pretrained params
-        new_env_model = define_model_from_scratch(env, config, logdir)
+        new_env_model = load_model(env, cfg, logdir)
 
         # create model object that loads the pretained params for
         # the feature extractor
@@ -91,20 +85,23 @@ if __name__ == "__main__":
                                                       new_env_model,
                                                       debug=True)
 
+    # pre-trained model without transfer learning
     elif do_resume_training and not is_transfer_learning:
         print("*" * 50)
         print(f"NOT TRANSFER LEARNING! Resuming training from {load_model_ckpt}!")
         print("*" * 50)
-        model = reload_model_from_ckpt(env, config, logdir)
+        model = load_model(env, cfg, logdir, load_model_ckpt)
 
+    # training model from scratch 
     else:
         print("*" * 50)
         print("Training from scratch!")
         print("*" * 50)
-        model = define_model_from_scratch(env, config, logdir)
+        model = load_model(env, cfg, logdir)
 
+    # create callback function to save best model
     callback = save_best_model.SaveOnBestTrainingRewardCallback(
-        check_freq=config['callback_params']['ckpt_freq'],
+        check_freq=cfg['callback_params']['ckpt_freq'],
         log_dir=logdir,
         verbose=1
     )
@@ -113,35 +110,40 @@ if __name__ == "__main__":
     env.reset()
 
     # print training info
-    print_training_info(config)
+    print_training_info(cfg)
 
-    savedir = config['output_params']['savedir']
-    model_algo = config['agent_params']['algo']
-    domain_name = config['env_params']['domain_name']
-    task_name = config['env_params']['task_name']
+    # more 
+    savedir = cfg['output_params']['savedir']
+    model_algo = cfg['agent_params']['algo']
+    domain_name = cfg['env_params']['domain_name']
+    task_name = cfg['env_params']['task_name']
 
+    # set checkpoint path 
     if not do_resume_training:
         callback.save_path = f"{callback.save_path}-{model_algo}-{domain_name}-{task_name}-{job_timestamp}"
     else:
         callback.save_path = load_model_ckpt
 
+    # print save name
     print("*" * 50)
     print(f"Saving best model with name {callback.save_path}")
     print("*" * 50)
 
     # train the agent
     model.learn(
-        total_timesteps=config['training_params']['total_training_steps'],
+        total_timesteps=cfg['training_params']['total_training_steps'],
         tb_log_name=f"{model_algo}-{domain_name}-{task_name}-{job_timestamp}",
         callback=callback,
         progress_bar=True
     )
 
+    # create save model directory
     savedir = os.path.join(savedir, domain_name, task_name)
     if not os.path.exists(savedir):
         os.makedirs(savedir, exist_ok=True)
 
-    savename = config['output_params']['savename']
+    # set save model name
+    savename = cfg['output_params']['savename']
     if not do_resume_training:
         if savename is None:
             savename = f"{model_algo}-{domain_name}-{task_name}-{job_timestamp}"
@@ -150,5 +152,6 @@ if __name__ == "__main__":
     else:
         savename = load_model_ckpt
 
+    # save trained model 
     model_savepath = os.path.join(savedir, savename)
     model.save(model_savepath)
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import os
-import re
-
+import time
 import torch
 import dmc2gym
 import numpy as np
@@ -12,19 +11,25 @@ from stable_baselines3.common.noise import NormalActionNoise
 from extractors import *
 from callbacks import *
 
-
 # Set device to GPU if available
 device = torch.device('cuda') if torch.cuda.is_available() \
     else torch.device('cpu')
 
-
-# create environment
+'''
+Create gym environment with specified config
+@param cfg - specified config
+@return gym environment
+'''
 def create_env(cfg):
-    '''Create Gym Env'''
     env = dmc2gym.make(**cfg['env_params'])
     return env
 
-
+'''
+Test gym environment for given number of steps
+Print out observation shapes and rewards
+@param env - gym environment
+@param num_steps - number of steps to run
+'''
 def test_env(env, num_steps=10):
     '''Tests pixel observations'''
     try:
@@ -37,26 +42,37 @@ def test_env(env, num_steps=10):
     except Exception as err:
         raise err
 
+'''
+Make log directory for specified config
+@param cfg - specified config 
+@return path to log directory
+'''
+def make_log_dir(cfg):
+    # get timestamp and values from config
+    job_timestamp = int(time.time())
+    model_algo = cfg['agent_params']['algo']
+    cnn_model_type = cfg['agent_params']['cnn_model_type']
+    domain_name = cfg['env_params']['domain_name']
+    task_name = cfg['env_params']['task_name']
 
-def make_log_dir(config, job_timestamp):
-    ''' Creates log directory '''
-    model_algo = config['agent_params']['algo']
-    cnn_model_type = config['agent_params']['cnn_model_type']
-    domain_name = config['env_params']['domain_name']
-    task_name = config['env_params']['task_name']
-
-    logdir = os.path.abspath(os.path.join(config['output_params']['logdir'],
+    # create log directory 
+    logdir = os.path.abspath(os.path.join(cfg['output_params']['logdir'],
                                           model_algo,
                                           cnn_model_type,
                                           domain_name,
                                           task_name,
                                           str(job_timestamp)))
+    # make and return logdir
     if not os.path.exists(logdir):
         os.makedirs(logdir, exist_ok=True)
     return logdir
 
-
-def get_extractor(agent_cfg, **kwargs):
+'''
+Get class of extractor network from specified config
+@param agent_cfg - agent config 
+@return class of specified extractor
+'''
+def get_extractor(agent_cfg):
     cnn_type = agent_cfg['cnn_model_type']
     cnns = {'resnet18': resnet18.CustomCNN,
             'resnet34': resnet34.CustomCNN,
@@ -64,40 +80,25 @@ def get_extractor(agent_cfg, **kwargs):
             'dmcnn2': dmcnn2.CustomCNN}
     return cnns[cnn_type]
 
-
-def print_training_info(cfg):
-    print('*' * 50)
-    print("\nBeginning Training")
-
-    for param_type, param_dict in cfg.items():
-        if isinstance(param_dict, dict):
-            print('*' * 50)
-            print(f"{param_type} Params\n")
-            print('*' * 50)
-
-            for param_name, param_val in param_dict.items():
-                print('*' * 50)
-                print(f"\tParam: {param_name} User Val: {param_val}\n")
-                print('*' * 50)
-
-    else:
-        print('*' * 50)
-        print(f"\tParam: {param_type} User Val: {param_dict}\n")
-        print('*' * 50)
-
-
-def define_policy_kwargs(env, config):
+'''
+Creates policy keyword arguments to pass to extractor
+from the specified env and config
+@param env - gym environment
+@param cfg - specified config
+@return policy keyword arguments created using config
+'''
+def define_policy_kwargs(env, cfg):
     policy_kwargs = {}
-    agent_config = config['agent_params']
+    agent_cfg = cfg['agent_params']
 
     # n_actions will be the dim of the output space of the network
     n_actions = env.action_space.shape[-1]
-    fc_features_dim = agent_config['fc_features_dim']
+    fc_features_dim = agent_cfg['fc_features_dim']
 
     # create custom feature extractor if required
-    if agent_config['cnn_model_type']:
+    if agent_cfg['cnn_model_type']:
         policy_kwargs = dict(
-            features_extractor_class=get_extractor(agent_config),
+            features_extractor_class=get_extractor(agent_cfg),
             features_extractor_kwargs=dict(
                 fc_features_dim=fc_features_dim,
             ),
@@ -105,54 +106,65 @@ def define_policy_kwargs(env, config):
             n_critics=1,
         )
     print(policy_kwargs)
-
     return policy_kwargs
 
-
-def define_model_from_scratch(env, config, logdir):
-
-    # get the configs
-    agent_config = config['agent_params']
-    training_config = config['training_params']
-
-    model_algo = config['agent_params']['algo']
-    domain_name = config['env_params']['domain_name']
-    task_name = config['env_params']['task_name']
+'''
+Creates a stable baselines 3 model for the specified
+env, config, and log directory. If checkpoint file is 
+specified, reloads model from there
+@param env - gym environment
+@param cfg - specified config
+@param logdir - given log directory
+@param load_model_ckpt - model checkpoint file
+@return stable baselines 3 model 
+'''
+def load_model(env, cfg, logdir, load_model_cpkt=None):
+    # get the configs and model algorithm
+    agent_cfg = cfg['agent_params']
+    training_cfg = cfg['training_params']
+    model_algo = cfg['agent_params']['algo']
 
     # create action noise
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions),
                                      sigma=0.1 * np.ones(n_actions))
 
-    train_freq = (training_config['train_freq_num'],
-                  training_config['train_freq_type'])
+    # get training frequency
+    train_freq = (training_cfg['train_freq_num'],
+                  training_cfg['train_freq_type'])
 
-    policy_kwargs = define_policy_kwargs(env, config)
+    # create policy kwargs
+    policy_kwargs = define_policy_kwargs(env, cfg)
+
+    # load model if specified, otherwise set model type
+    model_type = load_model_cpkt if load_model_cpkt else agent_cfg['model_type']
 
     if model_algo == "PPO":
         # remove n_critics param
         policy_kwargs.pop('n_critics')
-        if training_config['use_sde']:
+        
+        # Use SDE action noise
+        if training_cfg['use_sde']:
             action_noise = None
-            use_sde = training_config['use_sde']
+            use_sde = training_cfg['use_sde']
 
-        model = PPO(agent_config['model_type'],
+        model = PPO(model_type,
                     env,
                     verbose=1,
-                    learning_rate=training_config['learning_rate'],
-                    batch_size=training_config['batch_size'],
+                    learning_rate=training_cfg['learning_rate'],
+                    batch_size=training_cfg['batch_size'],
                     policy_kwargs=policy_kwargs,
                     use_sde=use_sde,
                     device=device,
                     tensorboard_log=logdir)
 
     elif model_algo == "DDPG":
-        model = DDPG(agent_config['model_type'],
+        model = DDPG(model_type,
                     env,
                     verbose=1,
-                    learning_rate=training_config['learning_rate'],
-                    batch_size=training_config['batch_size'],
-                    tau=agent_config['tau'],
+                    learning_rate=training_cfg['learning_rate'],
+                    batch_size=training_cfg['batch_size'],
+                    tau=agent_cfg['tau'],
                     policy_kwargs=policy_kwargs,
                     train_freq=train_freq,
                     device=device,
@@ -160,16 +172,17 @@ def define_model_from_scratch(env, config, logdir):
                     tensorboard_log=logdir)
 
     elif model_algo == "SAC":
-        if training_config['use_sde']:
+        # Use SDE action noise
+        if training_cfg['use_sde']:
             action_noise = None
-            use_sde = training_config['use_sde']
+            use_sde = training_cfg['use_sde']
 
-        model = SAC(agent_config['model_type'],
+        model = SAC(model_type,
                     env,
                     verbose=1,
-                    learning_rate=training_config['learning_rate'],
-                    batch_size=training_config['batch_size'],
-                    tau=agent_config['tau'],
+                    learning_rate=training_cfg['learning_rate'],
+                    batch_size=training_cfg['batch_size'],
+                    tau=agent_cfg['tau'],
                     train_freq=train_freq,
                     action_noise=action_noise,
                     use_sde=use_sde,
@@ -177,97 +190,25 @@ def define_model_from_scratch(env, config, logdir):
                     device=device,
                     tensorboard_log=logdir)
     else:
+        # invalid model
         raise (ValueError, f"invalid model algo provided {model_algo}. Only PPO, SAC, TD3 and DDPG are accepted")
 
     return model
 
-
-def reload_model_from_ckpt(env, config,
-                           load_model_ckpt, logdir):
-    # get the configs
-    agent_config = config['agent_params']
-    training_config = config['training_params']
-
-    model_algo = config['agent_params']['algo']
-    domain_name = config['env_params']['domain_name']
-    task_name = config['env_params']['task_name']
-
-    # create action noise
-    n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions),
-                                     sigma=0.1 * np.ones(n_actions))
-
-    train_freq = (training_config['train_freq_num'],
-                  training_config['train_freq_type'])
-
-    policy_kwargs = define_policy_kwargs(env, config)
-
-    if model_algo == "PPO":
-        policy_kwargs.pop('n_critics')
-        if training_config['use_sde']:
-            action_noise = None
-            use_sde = training_config['use_sde']
-
-        model = PPO.load(load_model_ckpt,
-                            env,
-                            learning_rate=training_config['learning_rate'],
-                            batch_size=training_config['batch_size'],
-                            verbose=1,
-                            use_sde=use_sde,
-                            policy_kwargs=policy_kwargs,
-                            device=device,
-                            tensorboard_log=logdir)
-
-    elif model_algo == "DDPG":
-        model = DDPG.load(load_model_ckpt,
-                            env,
-                            verbose=1,
-                            learning_rate=training_config['learning_rate'],
-                            batch_size=training_config['batch_size'],
-                            tau=agent_config['tau'],
-                            policy_kwargs=policy_kwargs,
-                            train_freq=train_freq,
-                            device=device,
-                            action_noise=action_noise,
-                            tensorboard_log=logdir)
-
-    elif model_algo == "SAC":
-        if training_config['use_sde']:
-            action_noise = None
-            use_sde = training_config['use_sde']
-
-        model = SAC.load(load_model_ckpt,
-                    env,
-                    verbose=1,
-                    learning_rate=training_config['learning_rate'],
-                    batch_size=training_config['batch_size'],
-                    tau=agent_config['tau'],
-                    train_freq=train_freq,
-                    action_noise=action_noise,
-                    use_sde=use_sde,
-                    policy_kwargs=policy_kwargs,
-                    device=device,
-                    tensorboard_log=logdir)
-    else:
-        raise (ValueError, f"invalid model algo provided {model_algo}. Only PPO, SAC, TD3 and DDPG are accepted")
-
-    return model
-
-
+'''
+Given a pretrained model and a new    
+'''
 def reload_model_from_ckpt_transfer_learn(pretrain_model,
                                           new_env_model,
                                           debug=False):
 
     keywords = ['features_extractor']
-    # keywords = ['features_extractor', 'log_std']
-
     pretrain_env_state_dict = pretrain_model.get_parameters()['policy']
     new_env_state_dict = new_env_model.get_parameters()['policy']
 
     if debug:
         print("*" * 50)
         print("New Env Model State Dict")
-        # print(type(new_env_model))
         print(new_env_model.get_parameters()['policy'].keys())
 
     pretrain_feat_ext_state_dict = OrderedDict()
@@ -278,9 +219,7 @@ def reload_model_from_ckpt_transfer_learn(pretrain_model,
     for key, weight in pretrain_env_state_dict.items():
         for keyword in keywords:
             if key.startswith(keyword):
-                # new_key = key.split('.', 1)[-1]
                 new_key = f"{key}"
-                # reload_model_state_dict[new_key] = weight
                 pretrain_feat_ext_state_dict['policy'][new_key] = weight
             else:
                 new_key = f"{key}"
@@ -303,3 +242,27 @@ def reload_model_from_ckpt_transfer_learn(pretrain_model,
     pretrain_model.set_parameters(pretrain_feat_ext_state_dict)
 
     return pretrain_model
+
+'''
+Prints out important info from specified config
+@param cfg - specified config
+'''
+def print_training_info(cfg):
+    print('*' * 50)
+    print("\nBeginning Training")
+
+    for param_type, param_dict in cfg.items():
+        if isinstance(param_dict, dict):
+            print('*' * 50)
+            print(f"{param_type} Params\n")
+            print('*' * 50)
+
+            for param_name, param_val in param_dict.items():
+                print('*' * 50)
+                print(f"\tParam: {param_name} User Val: {param_val}\n")
+                print('*' * 50)
+
+    else:
+        print('*' * 50)
+        print(f"\tParam: {param_type} User Val: {param_dict}\n")
+        print('*' * 50)
